93d369626a8c726b87f14873a0ea1a048f65958b
Oleg Stepanenko <olegst90@ukr.net>
2017-02-16 18:27:37 +0200

    Debug push script added

---
+++ push.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import MySQLdb
+from shutil import copytree
+
+import pushconfig
+
+print "Copying files..."
+dst = pushconfig.site_root + "/public/themes/" + pushconfig.in_name
+print "copying to " + dst
+copytree(".", dst)
+print "Copying layout to db..."
+db=MySQLdb.connect(host=pushconfig.db_host,user=pushconfig.db_user, passwd=pushconfig.db_pass,db=pushconfig.db_name)
+
+st = "update layout set content = %s where name = %s"
+
+file = file(pushconfig.in_file, "r")
+
+content = file.read()
+file.close()
+db.cursor().execute(st,[content, pushconfig.in_name])
